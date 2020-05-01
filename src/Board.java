24153ba7c480318fdac88db71d0c973d7dbb708a
Szuman <piszuman@gmail.com>
2020-05-01 19:45:04 +0200

    Utworzenie planszy

---
+++ src/Board.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JPanel {
+
+    private int[][] tab;
+    private int row;
+    private int coll;
+    private Board boardPanel;
+    private int pointSize;
+
+    public Board(int [][] tab, int row, int coll, Board boardPanel, int pointSize) {
+        this.row = row;
+        this.coll = coll;
+        this.tab = tab;
+        this.boardPanel = boardPanel;
+        this.pointSize = pointSize;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < coll; j++) {
+                if (tab[i][j] == 0) {
+                    g.setColor(Color.black);
+                    g.fillRect(i * pointSize, j * pointSize, pointSize, pointSize);
+                }
+                else if (tab[i][j] == 1) {
+                    g.setColor(Color.yellow);
+                    g.fillRect(i * pointSize, j * pointSize, pointSize, pointSize);
+                }
+                else if (tab[i][j] == 2) {
+                    g.setColor(Color.blue);
+                    g.fillRect(i * pointSize, j * pointSize, pointSize, pointSize);
+                }
+                else if (tab[i][j] == 3) {
+                    g.setColor(Color.red);
+                    g.fillRect(i * pointSize, j * pointSize, pointSize, pointSize);
+                }
+            }
+        }
+    }
+}
--- src/GUI.java
+++ src/GUI.java
@@ -1,12 +1,86 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Hashtable;
 
 public class GUI {
-JFrame frame;
+    JFrame frame;
+    Board board;
 
-public void createGUI() {
-    frame = new JFrame("WireWorld");
-    frame.setSize(800, 600);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setVisible(true);
+    final int boardSize = 50;
+    final int pointSize = 10;
+    final int tab[][] = new int[boardSize][boardSize];
+    final int mouseButtonLeft = 1;
+    final int mouseButtonRight = 3;
+    final int frameSize = (boardSize + 1) * pointSize + 3;
+
+    public void createGUI() {
+        frame = new JFrame("WireWorld");
+        frame.setSize(frameSize + 3, frameSize + 105);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        options();
+        frame.setVisible(true);
+        }
+
+    public void options() {
+        JButton stepB = new JButton("Step");
+        JButton clearB = new JButton("Clear");
+        JButton saveB = new JButton("Save");
+        JButton loadB = new JButton("Load");
+        JToggleButton playB = new JToggleButton("Play");
+        JSlider fastS = new JSlider(JSlider.HORIZONTAL, 1, 700, 350);
+
+        Hashtable lTable = new Hashtable();
+        lTable.put(50, new JLabel("Slow"));
+        lTable.put(650, new JLabel("Fast"));
+        fastS.setLabelTable(lTable);
+        fastS.setPaintLabels(true);
+
+        if (playB.isSelected())
+            playB.setText("Stop");
+        else
+            playB.setText("Play");
+
+        JPanel controlPanel = new JPanel();
+        controlPanel.add(stepB);
+        controlPanel.add(fastS);
+        controlPanel.add(playB);
+        controlPanel.add(clearB);
+
+        JPanel optFilePanel = new JPanel();
+        optFilePanel.add(saveB);
+        optFilePanel.add(loadB);
+
+        board = new Board(tab, boardSize, boardSize, board, pointSize);
+        board.setBackground(Color.white);
+
+        board.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                int x = e.getX() / 10;
+                int y = e.getY() / 10;
+                if (e.getButton() == mouseButtonLeft) {
+                    if (tab[x][y] != 1)
+                        tab[x][y] = 1;
+                    else
+                        tab[x][y] = 0;
+                }
+                if (e.getButton() == mouseButtonRight) {
+                    if (tab[x][y] != 2)
+                        tab[x][y] = 2;
+                    else
+                        tab[x][y] = 3;
+                }
+                board.repaint();
+            }
+        });
+
+        frame.getContentPane().add(BorderLayout.NORTH ,controlPanel);
+        frame.getContentPane().add(BorderLayout.SOUTH, optFilePanel);
+        frame.getContentPane().add(BorderLayout.CENTER, board);
+
     }
+
 }
