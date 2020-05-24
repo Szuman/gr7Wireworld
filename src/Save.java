41a28075fbd40d886579cffd67a52b7c5c1bc1ad
karnialv <61946908+karnialv@users.noreply.github.com>
2020-05-24 19:00:45 +0200

    Zmieniony Save (mozemy wybierac nazwe i miejsce pliku)

--- src/Save.java
+++ src/Save.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
 import javax.swing.filechooser.*;
+import java.awt.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class Save {
+public class Save extends Component {
     private int[][] tab;
     private int row;
     private int coll;
@@ -23,34 +24,35 @@ public class Save {
     }
 
     public void save() {
-        File myFile = new File("example.txt");
+        JFileChooser fc = new JFileChooser();
+        fc.showSaveDialog(this);
+        File f = fc.getSelectedFile();
         try {
-            FileWriter writer = new FileWriter(myFile);
+            FileWriter fw = new FileWriter(f);
             for (int i = 0; i < row; i++) {
                 for (int j = 0; j < coll; j++) {
                     if ((tab[i][j]) == 1) {
                         String text = "Yellow i=" + i + ", j=" + j + ";";
-                        writer.write(text);
-                        writer.append('\n');
-                        writer.flush();
+                        fw.write(text);
+                        fw.append('\n');
+                        fw.flush();
                     }
                     if ((tab[i][j]) == 2) {
                         String text = "Red i=" + i + ", j=" + j + ";";
-                        writer.write(text);
-                        writer.append('\n');
-                        writer.flush();
+                        fw.write(text);
+                        fw.append('\n');
+                        fw.flush();
                     }
                     if ((tab[i][j]) == 3) {
                         String text = "Blue i=" + i + ", j=" + j + ";";
-                        writer.write(text);
-                        writer.append('\n');
-                        writer.flush();
+                        fw.write(text);
+                        fw.append('\n');
+                        fw.flush();
                     }
                 }
-            }
+            } fw.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 }
-
